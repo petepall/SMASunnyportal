549186b3d24a04369560835669bbe9ad760b22e2
Peter Pallen <peter@ppallen.be>
2022-10-22 22:15:28 +0200

    Changes: Added IPlantProfile

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,45 @@
-export interface Token {
+export interface IToken {
 	secret_key: string;
 	identifier: string;
 }
 
-export interface PlantList {
+export interface IPlantList {
 	plantname: string;
 	plantoid: string;
+}
+
+export interface IPlantProfile {
+	plantHeader: {
+		plantname: string;
+		peakpower: number;
+		powerunit: string;
+		location: string;
+		startData: string;
+		description: string;
+		plantImage: string;
+		plantImageHight: number;
+		plantImageWidth: number;
+	};
+	expectedPlantProduction: {
+		expectedYield: string;
+		expectedYieldUnit: string;
+		expectedCO2Reduction: string;
+		expectedCO2ReductionUnit: string;
+	};
+	modules: {
+		moduleName: string;
+		numerOfModules: number;
+		alignment: number;
+		gradient: number;
+	};
+	inverters: {
+		inverterName: string;
+		numberOfInverters: number;
+		icon: string;
+	};
+	communicationProducts: {
+		communicationProductName: string;
+		numberOfCommunicationProducts: number;
+		icon: string;
+	};
 }
