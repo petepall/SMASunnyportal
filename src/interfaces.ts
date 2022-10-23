f9fb72af24f110106e1dcdccd77f691281b55eeb
Peter Pallen <peter@ppallen.be>
2022-10-23 16:59:01 +0200

    Changes: pulled subobjects from IPlantProfile

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -25,6 +25,72 @@ export interface IPlantList {
 }
 
 /**
+ * Interface representing the inverters used in a plant.
+ * @date 23/10/2022 - 16:56:22
+ *
+ * @interface IInverters
+ * @typedef {IInverters}
+ */
+interface IInverters {
+	inverterName: string;
+	numberOfInverters: number;
+	icon: string;
+}
+
+/**
+ * Inteface representing the communication products used in a plant.
+ * @date 23/10/2022 - 16:56:41
+ *
+ * @interface ICommunicationsProducts
+ * @typedef {ICommunicationsProducts}
+ */
+interface ICommunicationsProducts {
+	communicationProductName: string;
+	numberOfCommunicationProducts: number;
+	icon: string;
+}
+
+/**
+ * Interface representing the solar panels, alignment and orientation used in a plant.
+ * @date 23/10/2022 - 16:56:59
+ *
+ * @interface IModules
+ * @typedef {IModules}
+ */
+interface IModules {
+	moduleName: string;
+	numerOfModules: number;
+	alignment: number;
+	gradient: number;
+}
+
+/**
+ * Interface representing the expectied yearly production of a plant.
+ * @date 23/10/2022 - 16:57:53
+ *
+ * @interface IExpectedPlantProduction
+ * @typedef {IExpectedPlantProduction}
+ */
+interface IExpectedPlantProduction {
+	expectedYield: string;
+	expectedYieldUnit: string;
+	expectedCO2Reduction: string;
+	expectedCO2ReductionUnit: string;
+}
+
+interface IPlantHeader {
+	plantname: string;
+	peakpower: number;
+	powerunit: string;
+	location: string;
+	startData: string;
+	description: string;
+	plantImage: string;
+	plantImageHight: number;
+	plantImageWidth: number;
+}
+
+/**
  * Interface representing the plant information object from the sunny portal API.
  * @date 22/10/2022 - 23:32:58
  *
@@ -34,36 +100,18 @@ export interface IPlantList {
  */
 export interface IPlantProfile {
 	plantHeader: {
-		plantname: string;
-		peakpower: number;
-		powerunit: string;
-		location: string;
-		startData: string;
-		description: string;
-		plantImage: string;
-		plantImageHight: number;
-		plantImageWidth: number;
+		[key: string]: IPlantHeader;
 	};
 	expectedPlantProduction: {
-		expectedYield: string;
-		expectedYieldUnit: string;
-		expectedCO2Reduction: string;
-		expectedCO2ReductionUnit: string;
+		[key: string]: IExpectedPlantProduction;
 	};
 	modules: {
-		moduleName: string;
-		numerOfModules: number;
-		alignment: number;
-		gradient: number;
+		[key: string]: IModules;
 	};
 	inverters: {
-		inverterName: string;
-		numberOfInverters: number;
-		icon: string;
+		[key: string]: IInverters;
 	};
 	communicationProducts: {
-		communicationProductName: string;
-		numberOfCommunicationProducts: number;
-		icon: string;
+		[key: string]: ICommunicationsProducts;
 	};
 }
