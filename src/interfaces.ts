ddaa7aae101956389167a858bd14620a22a6aa22
Peter Pallen <peter@ppallen.be>
2022-10-24 13:21:12 +0200

    Changes: added docstring

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -123,6 +123,14 @@ export interface IPlantProfile {
 	};
 }
 
+/**
+ * Interface representing the configuration object for the sunny portal API.
+ * @date 24/10/2022 - 13:19:38
+ *
+ * @export
+ * @interface ISunnyConfig
+ * @typedef {ISunnyConfig}
+ */
 export interface ISunnyConfig {
 	Login: {
 		email: string;
