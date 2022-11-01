9bdfeb3c5fc0b7dfe075141a0340c4931a5fc88a
Peter Pallen <peter@ppallen.be>
2022-11-01 13:46:32 +0100

    Changes: updates the class, method and function calls

--- src/index.ts
+++ src/index.ts
@@ -69,10 +69,10 @@ async function logout(): Promise<void> {
 
 /**
  * Retrieve the list of plants and return the plant id and name as object.
- * @date 21/10/2022 - 22:02:48
+ * @date 01/11/2022 - 13:38:26
  *
  * @async
- * @returns {Promise<any>}
+ * @returns {Promise<IPlantList>}
  */
 async function parseJSONPlantList(): Promise<IPlantList> {
 	const request = new PlantListRequest(
@@ -96,21 +96,20 @@ async function parseJSONPlantList(): Promise<IPlantList> {
 
 /**
  * Retrieve the plant information based on the plantID and return the data as a JSON object.
- * @date 23/10/2022 - 17:13:59
+ * @date 01/11/2022 - 13:38:50
  *
  * @async
- * @param {AxiosInstance} conn
- * @param {IToken} token
  * @param {string} plantId
  * @returns {Promise<any>}
  */
-async function parseJSONPlantData(conn: AxiosInstance, token: IToken, plantId: string): Promise<any> {
+async function parseJSONPlantData(plantId: string): Promise<any> {
 	const request = new PlantProfileRequest(
 		'plant',
 		'GET',
+		conn,
 		token
 	);
-	const plantData = await request.getPlantData(conn, token, plantId);
+	const plantData = await request.getPlantData(plantId);
 
 	let data = null;
 	parser.parseString(plantData, (err: any, result: any) => {
@@ -123,21 +122,20 @@ async function parseJSONPlantData(conn: AxiosInstance, token: IToken, plantId: s
 
 /**
  * Retrieve the list of devices for a plant and return the device list as a JSON object.
- * @date 25/10/2022 - 14:40:38
+ * @date 01/11/2022 - 13:39:08
  *
  * @async
- * @param {AxiosInstance} conn
- * @param {IToken} token
  * @param {string} plantId
  * @returns {Promise<any>}
  */
-async function parseJSONPlantDeviceListData(conn: AxiosInstance, token: IToken, plantId: string): Promise<any> {
+async function parseJSONPlantDeviceListData(plantId: string): Promise<any> {
 	const request = new PlantDeviceListRequest(
 		'device',
 		'GET',
+		conn,
 		token
 	);
-	const plantDeviceListData = await request.getPlantDeviceListData(conn, token, plantId);
+	const plantDeviceListData = await request.getPlantDeviceListData(plantId);
 
 	let data = null;
 	parser.parseString(plantDeviceListData, (err: any, result: any) => {
@@ -150,22 +148,21 @@ async function parseJSONPlantDeviceListData(conn: AxiosInstance, token: IToken, 
 
 /**
  * Retrieve the device parameters for a given deviceID and return the device parameters as a JSON object.
- * @date 25/10/2022 - 14:41:50
+ * @date 01/11/2022 - 13:39:29
  *
  * @async
- * @param {AxiosInstance} conn
- * @param {IToken} token
  * @param {string} plantId
  * @param {string} deviceId
  * @returns {Promise<any>}
  */
-async function parseJSONPlantDeviceParameterData(conn: AxiosInstance, token: IToken, plantId: string, deviceId: string): Promise<any> {
+async function parseJSONPlantDeviceParameterData(plantId: string, deviceId: string): Promise<any> {
 	const request = new PlantDeviceParametersRequest(
 		'device',
 		'GET',
+		conn,
 		token
 	);
-	const plantDeviceParameterData = await request.getPlantDeviceParametersData(conn, token, plantId, deviceId);
+	const plantDeviceParameterData = await request.getPlantDeviceParametersData(plantId, deviceId);
 
 	let data = null;
 	parser.parseString(plantDeviceParameterData, (err: any, result: any) => {
@@ -176,13 +173,23 @@ async function parseJSONPlantDeviceParameterData(conn: AxiosInstance, token: ITo
 	return data;
 }
 
-async function parseJSONLastDataExactData(conn: AxiosInstance, token: IToken, plantID: string, date: string): Promise<any> {
+/**
+ * Method to retrieve the last exact data from the Sunny Portal API.
+ * @date 01/11/2022 - 13:41:35
+ *
+ * @async
+ * @param {string} date
+ * @returns {Promise<any>}
+ */
+async function parseJSONLastDataExactData(date: string): Promise<any> {
 	const request = new DataRequest(
 		'data',
 		'GET',
-		token
+		conn,
+		token,
+		plantoid,
 	);
-	const lastDataExactData = await request.getLastDataExactData(conn, token, plantID, date);
+	const lastDataExactData = await request.getLastDataExactData(date);
 
 	let data = null;
 	parser.parseString(lastDataExactData, (err: any, result: any) => {
@@ -193,13 +200,24 @@ async function parseJSONLastDataExactData(conn: AxiosInstance, token: IToken, pl
 	return data;
 }
 
-async function parseJSONAllDataRequestData(conn: AxiosInstance, token: IToken, plantID: string, date: string, interval: string): Promise<any> {
+/**
+ * Method to retrieve all the generation data on a specific date from the Sunny Portal API.
+ * @date 01/11/2022 - 13:42:37
+ *
+ * @async
+ * @param {string} date
+ * @param {string} interval
+ * @returns {Promise<any>}
+ */
+async function parseJSONAllDataRequestData(date: string, interval: string): Promise<any> {
 	const request = new DataRequest(
 		'data',
 		'GET',
-		token
+		conn,
+		token,
+		plantoid
 	);
-	const allDataRequestData = await request.getAllDataRequestData(conn, token, plantID, date, interval);
+	const allDataRequestData = await request.getAllDataRequestData(date, interval);
 
 	let data = null;
 	parser.parseString(allDataRequestData, (err: any, result: any) => {
@@ -210,13 +228,25 @@ async function parseJSONAllDataRequestData(conn: AxiosInstance, token: IToken, p
 	return data;
 }
 
-async function parseJSONDayOverviewRequestData(conn: AxiosInstance, token: IToken, plantID: string, date: string, quarter: boolean, include_all: boolean): Promise<any> {
+/**
+ * Method to retrieve the day overview by day or quarter hour from the Sunny Portal API.
+ * @date 01/11/2022 - 13:43:39
+ *
+ * @async
+ * @param {string} date
+ * @param {boolean} quarter
+ * @param {boolean} include_all
+ * @returns {Promise<any>}
+ */
+async function parseJSONDayOverviewRequestData(date: string, quarter: boolean, include_all: boolean): Promise<any> {
 	const request = new DataRequest(
 		'data',
 		'GET',
-		token
+		conn,
+		token,
+		plantoid
 	);
-	const allDataRequestData = await request.getDayOverviewRequestData(conn, token, plantID, date, quarter, include_all);
+	const allDataRequestData = await request.getDayOverviewRequestData(date, quarter, include_all);
 
 	let data = null;
 	parser.parseString(allDataRequestData, (err: any, result: any) => {
@@ -267,7 +297,7 @@ const conn = axios.create({
 const token = await getToken(sunnyConfig.Login.email, sunnyConfig.Login.password);
 const plantlist = await parseJSONPlantList();
 const plantoid = plantlist.plantoid;
-const plantData = await parseJSONPlantData(conn, token, plantoid);
+const plantData = await parseJSONPlantData(plantoid);
 
 const plantProfile: IPlantProfile = {
 	plantHeader: {
@@ -335,25 +365,25 @@ for (const key in communicationProducts.communicationProduct) {
 }
 // console.dir(plantProfile);
 
-const plantDeviceListData = await parseJSONPlantDeviceListData(conn, token, plantoid);
+const plantDeviceListData = await parseJSONPlantDeviceListData(plantoid);
 // console.dir(plantDeviceListData);
 
-const plantDeviceParameterData = await parseJSONPlantDeviceParameterData(conn, token, plantoid, plantDeviceListData.device[0].$.oid);
+const plantDeviceParameterData = await parseJSONPlantDeviceParameterData(plantoid, plantDeviceListData.device[0].$.oid);
 // for (const key in plantDeviceParameterData.service.parameterlist.parameter) {
 // 	console.dir(plantDeviceParameterData.service.parameterlist.parameter[key]);
 // }
 
-const lastDataExactData = await parseJSONLastDataExactData(conn, token, plantoid, (new Date()).toISOString().slice(0, 10));
+const lastDataExactData = await parseJSONLastDataExactData((new Date()).toISOString().slice(0, 10));
 // for (const key in lastDataExactData.service.data.Energy) {
 // 	console.dir(lastDataExactData.service.data.Energy[key]);
 // }
 
-const allDataRequestData = await parseJSONAllDataRequestData(conn, token, plantoid, (new Date()).toISOString().slice(0, 10), 'month');
+const allDataRequestData = await parseJSONAllDataRequestData((new Date()).toISOString().slice(0, 10), 'month');
 // for (const key in allDataRequestData.service.data.Energy.channel) {
 // 	console.dir(allDataRequestData.service.data.Energy.channel[key].month);
 // }
 
-const dayOverviewRequestData = await parseJSONDayOverviewRequestData(conn, token, plantoid, (new Date()).toISOString().slice(0, 10), true, true);
+const dayOverviewRequestData = await parseJSONDayOverviewRequestData((new Date()).toISOString().slice(0, 10), true, true);
 // console.dir(dayOverviewRequestData.service.data['overview-day-fifteen-total'].channel);
 // for (const key in dayOverviewRequestData.service.data['overview-day-fifteen-total'].channel) {
 // 	console.dir(dayOverviewRequestData.service.data['overview-day-fifteen-total'].channel[key].day.fiveteen);
